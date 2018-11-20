c573d14a2cf559bf5daae822e2cc2252bc49b8ac
Miguel Angel Córdova Apolinario <cordovauni@gmail.com>
2018-11-20 08:38:31 -0500

    -

--- app/src/main/java/com/example/rocio/meetandtravel/viewcontrollers/activities/CreateProvidersActivity.kt
+++ app/src/main/java/com/example/rocio/meetandtravel/viewcontrollers/activities/CreateProvidersActivity.kt
@@ -2,6 +2,7 @@ package com.example.rocio.meetandtravel.viewcontrollers.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rocio.meetandtravel.R
 
 class CreateProvidersActivity : AppCompatActivity() {
 
